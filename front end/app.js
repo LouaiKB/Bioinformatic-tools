a841983d3509f182a49d6fbe60936c225fa228ef
LouaiKB <57999863+LouaiKB@users.noreply.github.com>
2020-08-20 16:41:33 +0200

    Add files via upload

---
+++ front end/app.js
@@ -0,0 +1,36 @@
+const containers = document.querySelectorAll('.container');
+	containers.forEach(element => {
+		element.addEventListener('mouseenter', () => {
+			element.style.cssText = 'width: 300px; height: 200px; display: flex; align-items: center; justify-content: center;'
+		});
+		element.addEventListener('mouseleave', () => {
+			element.style.cssText = 'width: 250px; height: 150px';
+		});
+	});
+/*
+const myCustomDiv = document.createElement('div');
+
+function respondToTheClick(evt) {
+	if(evt.target.nodeName === 'P') {
+		console.log('A paragraph was clicked: ' + evt.target.textContent);
+	}
+}
+for (let i = 1; i <= 200; i++) {
+    const newElement = document.createElement('p');
+    newElement.innerHTML = '<p>This is paragraph number</p><span>' + i + '</span>';
+
+    myCustomDiv.appendChild(newElement);
+}
+
+document.body.appendChild(myCustomDiv);
+
+
+myCustomDiv.addEventListener('click', respondToTheClick);
+
+/*
+document.querySelector('#content').addEventListener('click', function (evt) {
+    if (evt.target.nodeName === 'SPAN') {  // ← verifies target is desired element
+        console.log('A span was clicked with text ' + evt.target.textContent);
+    }
+});
+*/ 
