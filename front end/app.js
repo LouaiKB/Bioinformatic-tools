49b9ca1206910b779bd63bd2182a6d8fc343bee2
LouaiKB <loukb1998@gmail.com>
2020-08-22 14:46:19 +0100

    start Django!

--- front end/app.js
+++ front end/app.js
@@ -1,36 +1,40 @@
 const containers = document.querySelectorAll('.container');
-	containers.forEach(element => {
-		element.addEventListener('mouseenter', () => {
-			element.style.cssText = 'width: 300px; height: 200px; display: flex; align-items: center; justify-content: center;'
-		});
-		element.addEventListener('mouseleave', () => {
-			element.style.cssText = 'width: 250px; height: 150px';
-		});
+
+containers.forEach(element => {
+	
+	element.addEventListener('mouseenter', (evt) => {
+		evt.target.style.cssText = 'width: 260px; height: 160px; display: flex; align-items: center; justify-content: center;';
 	});
-/*
-const myCustomDiv = document.createElement('div');
+	
+	element.addEventListener('mouseleave', (evt) => {
+		evt.target.style.cssText = 'width: 250px; height: 150px';
+	});
 
-function respondToTheClick(evt) {
-	if(evt.target.nodeName === 'P') {
-		console.log('A paragraph was clicked: ' + evt.target.textContent);
-	}
-}
-for (let i = 1; i <= 200; i++) {
-    const newElement = document.createElement('p');
-    newElement.innerHTML = '<p>This is paragraph number</p><span>' + i + '</span>';
-
-    myCustomDiv.appendChild(newElement);
-}
-
-document.body.appendChild(myCustomDiv);
-
-
-myCustomDiv.addEventListener('click', respondToTheClick);
-
-/*
-document.querySelector('#content').addEventListener('click', function (evt) {
-    if (evt.target.nodeName === 'SPAN') {  // ← verifies target is desired element
-        console.log('A span was clicked with text ' + evt.target.textContent);
-    }
 });
-*/ 
+
+// const myCustomDiv = document.createElement('div');
+
+// function respondToTheClick(evt) {
+// 	if(evt.target.nodeName === 'P') {
+// 		console.log('A paragraph was clicked: ' + evt.target.textContent);
+// 	}
+// }
+// for (let i = 1; i <= 200; i++) {
+//     const newElement = document.createElement('p');
+//     newElement.innerHTML = '<p>This is paragraph number</p><span>' + i + '</span>';
+
+//     myCustomDiv.appendChild(newElement);
+// }
+
+// document.body.appendChild(myCustomDiv);
+
+
+// myCustomDiv.addEventListener('click', respondToTheClick);
+
+
+// document.querySelector('#content').addEventListener('click', function (evt) {
+//     if (evt.target.nodeName === 'SPAN') {  // ← verifies target is desired element
+//         console.log('A span was clicked with text ' + evt.target.textContent);
+//     }
+// });
+// 
